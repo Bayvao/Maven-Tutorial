c80626304c951441d3ab434db0e868c023c3b02d
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2021-06-17 22:04:51 +0530

    junit5 failsafe

---
+++ maven-testing-example/src/test/java/com/maven/testing/JavaHelloWorldIT.java
@@ -0,0 +1,11 @@
+package com.maven.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("My IT Ran");
+    }
+}
